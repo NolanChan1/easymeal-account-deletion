65b305dc1ccc5ce36bcba19e42c50820503dec51
NolanChan1 <nolanchan1@gmail.com>
2023-12-25 11:48:24 -0700

    env file configuration

--- app/firebase-config.ts
+++ app/firebase-config.ts
@@ -1,12 +1,11 @@
 import { initializeApp, getApp, getApps, FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-import { firebaseConfig } from "./fb-config";
+//import { firebaseConfig } from "./fb-config";
 
 // Issues with reading environment variables for firebase config variables
-/*
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAjHu_97x7kVvmZxZ4vO2qwYbQvszkiL7I",
+  apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
@@ -14,7 +13,6 @@ const firebaseConfig: FirebaseOptions = {
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
 };
-*/
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
